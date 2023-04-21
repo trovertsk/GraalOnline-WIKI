eab780351970049ee06ab6f88ac065baf3a9189e
Trevortsk <107767308+trovertsk@users.noreply.github.com>
2023-04-21 16:47:05 -0300

    Add files via upload

---
+++ Meu Wiki/codigo.php
@@ -0,0 +1,5 @@
+<?php
+  if(isset($_GET['pesquisa'])) {
+    // código para processar a pesquisa aqui
+  }
+?>
---
+++ Meu Wiki/script.js
@@ -0,0 +1,6 @@
+function toggleTheme() {
+    var body = document.querySelector('body');
+    body.classList.toggle('light-mode');
+    body.classList.toggle('dark-mode');
+  }
+  
